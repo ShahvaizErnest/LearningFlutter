716446a28541f0f259b2aa5d7acc3bdbe27a6099
Shahvaiz Ernest <shahvaizernest@gmail.com>
2023-01-04 19:36:27 +0500

    Day 4 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,19 +5,50 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
     return Material(
-      // ignore: prefer_const_constructors
-      child: Center(
+        color: Colors.white,
         // ignore: prefer_const_constructors
-        child: Text(
-          "Login Page",
-          // ignore: prefer_const_constructors
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.deepPurple,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            // ignore: prefer_const_constructors
+            Text(
+              "Welcome",
+              // ignore: prefer_const_constructors
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 25.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    // ignore: prefer_const_constructors
+                    decoration: InputDecoration(
+                        hintText: "Enter Username", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    // ignore: prefer_const_constructors
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter Password", labelText: "Password"),
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("Hi Shahvaiz");
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
