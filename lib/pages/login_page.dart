544656ae2b27430f1da7a0a0df69a5f8ee281072
Shahvaiz Ernest <shahvaizernest@gmail.com>
2023-01-05 20:51:03 +0500

    Day 6 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,18 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utilities/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
+
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
@@ -20,7 +31,7 @@ class LoginPage extends StatelessWidget {
               ),
               // ignore: prefer_const_constructors
               Text(
-                "Welcome",
+                "Welcome $name",
                 // ignore: prefer_const_constructors
                 style: TextStyle(
                   fontSize: 24,
@@ -37,6 +48,10 @@ class LoginPage extends StatelessWidget {
                         // ignore: prefer_const_constructors
                         decoration: InputDecoration(
                             hintText: "Enter Username", labelText: "Username"),
+                        onChanged: (value) {
+                          name = value;
+                          setState(() {});
+                        },
                       ),
                       TextFormField(
                         // ignore: prefer_const_constructors
@@ -45,16 +60,49 @@ class LoginPage extends StatelessWidget {
                             hintText: "Enter Password", labelText: "Password"),
                       ),
                       Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 60),
-                        child: ElevatedButton(
-                          child: Text("Login"),
-                          style:
-                              TextButton.styleFrom(minimumSize: Size(150, 40)),
-                          onPressed: () {
+                        padding: const EdgeInsets.symmetric(vertical: 40),
+                        child: InkWell(
+                          onTap: () async {
+                            setState(() {
+                              changeButton = true;
+                            });
+                            await Future.delayed(Duration(seconds: 1));
                             Navigator.pushNamed(context, MyRoutes.homeRoute);
                           },
+                          child: AnimatedContainer(
+                            duration: Duration(seconds: 1),
+                            width: changeButton ? 45 : 140,
+                            height: 45,
+                            alignment: Alignment.center,
+                            child: changeButton
+                                ? Icon(
+                                    Icons.done,
+                                    color: Colors.white,
+                                  )
+                                : Text(
+                                    "Login",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple,
+                                borderRadius: BorderRadius.circular(
+                                    changeButton ? 45 : 8)),
+                          ),
                         ),
                       ),
+                      // Padding(
+                      //   padding: const EdgeInsets.symmetric(vertical: 60),
+                      //   child: ElevatedButton(
+                      //     child: Text("Login"),
+                      //     style:
+                      //         TextButton.styleFrom(minimumSize: Size(150, 40)),
+                      //     onPressed: () {
+                      //       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      //     },
+                      //   ),
+                      // ),
                     ],
                   ),
                 ),
